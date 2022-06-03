791f591818667a996525d9332d16d13c48e18e3f
Carolina Monden <64371781+CarolinaMonden@users.noreply.github.com>
2022-06-03 14:50:50 -0300

    Add files via upload

---
+++ TRABALHO_IOT.ino
@@ -0,0 +1,156 @@
+#include "DHT.h"
+#include <ESP8266WiFi.h> //inclui suporte ao NodeMCU
+#include <PubSubClient.h> //inclui suporte ao MQTT no HiveMQ Cloud
+
+// Configuracoes da rede WiFi
+const char* ssid = "Suze"; //SSID da rede WiFi
+const char* password = "manurafa"; //senha da rede WiFi
+
+const char* mqtt_server = "98571f1b42354f9da3be4ff37ad39065.s1.eu.hivemq.cloud"; //URL do broker MQTT
+const int mqtt_server_port = 8883; //porta do broker MQTT
+
+// Variaveis globais e objetos
+#define MSG_BUFFER_SIZE (500) //define MSG_BUFFER_SIZE como 500
+#define DHTPIN D6 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+#define LED D7
+WiFiClient client; //cria o objeto client
+PubSubClient mqtt_client(client); //cria o objeto mqtt_client
+long lastMsg = 0;
+
+String clientID = "ESP8266Client-"; //identificacao do cliente
+
+String topicoPrefixo = "MACK32027494"; //para o prefixo do topico, utilizar MACK seguido do TIA
+String topicoTodos = topicoPrefixo + "/#"; //para retornar todos os topicos
+String topico_0 = topicoPrefixo + "/hello"; //topico para teste
+String mensagem_0 = "NodeMCU Connected"; //mensagem para o topico 0
+String topico_1 = topicoPrefixo + "/sensor1"; //topico para o sensor 1
+String mensagem_1 = ""; //mensagem para o topico 1
+String topico_2 = topicoPrefixo + "/atuador1"; //topico para o atuador 1
+String mensagem_2 = ""; //mensagem para o topico 2
+String mensagemTemp = ""; //armazena temporariamente as mensagens recebidas via MQTT
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// Funcao para configurar a conexao WiFi
+void setup_wifi() {
+  //WiFi.mode(WIFI_STA); //ativa o modo de estacao
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+    Serial.println();
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // Funcao para receber mensagens
+void callback(char* topic, byte* payload, unsigned int length) {
+  mensagemTemp = "";
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+    mensagemTemp += (char)payload[i];
+  }
+
+  if (String(topic) == topico_2) {
+    mensagem_2 = mensagemTemp;
+  }
+
+  Serial.println();
+  
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// Funcao para conectar no broker MQTT
+void reconnect() {
+  // Loop until we’re reconnected
+  while (!mqtt_client.connected()) {
+    Serial.print("Attempting MQTT connection…");
+
+  // Create a random client ID
+    randomSeed(micros()); //inicializa a semente do gerador de numeros aleatorios
+    clientID += String(random(0xffff), HEX);
+
+  // Attempt to connect
+    if (mqtt_client.connect(clientID.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqtt_client.publish(topico_0.c_str(), mensagem_0.c_str());
+      // ... and resubscribe
+      mqtt_client.subscribe(topicoTodos.c_str());
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+} 
+ 
+void setup() 
+{
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  dht.begin();
+  setup_wifi();
+  mqtt_client.setServer(mqtt_server, mqtt_server_port); //configura a conexao com o broker MQTT
+  mqtt_client.setCallback(callback); //configura o recebimento de mensagens
+}
+ 
+void loop() {
+  if (!mqtt_client.connected()) {
+    reconnect();
+  }
+  mqtt_client.loop(); //processa as mensagens e mantem a conexao com o broker MQTT
+  
+  pinMode(D7, OUTPUT);
+  delay(2000);
+  
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(t) || isnan(h)) {
+    Serial.println("Failed to read from DHT");
+    return;
+  } 
+    Serial.print("Umidade: ");
+    Serial.print(h);
+    Serial.print("     ");
+    Serial.print("Temperatura: ");
+    Serial.print(t);
+    Serial.println(" C");
+  
+
+   if (t > 10){
+    Serial.println("A estufa está com a temperatura abaixo do ideal! A led foi acionada para seu aquecimento!");
+    digitalWrite(D7, HIGH);
+    delay(2000);
+    mensagem_1 = "A estufa está com a temperatura abaixo do ideal! A led foi acionada para seu aquecimento!";
+  }
+  else if(t > 20){
+    Serial.println("A estufa está com a temperatura acima do ideal!");
+    digitalWrite(D7, LOW);
+    delay(2000);
+    mensagem_2 = "A estufa está com a temperatura acima do ideal!";
+  }
+   //Publica mensagem
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    mqtt_client.publish(topico_1.c_str(), mensagem_1.c_str());
+  }
+
+
+}
